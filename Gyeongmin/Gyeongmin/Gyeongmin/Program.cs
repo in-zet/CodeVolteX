f6b99393ea5891b74ceafa7444d2e9440305893c
DAL_Tohko <happywhrudals0203@gmail.com>
2025-01-22 20:56:37 +0900

    1-1

--- Gyeongmin/Gyeongmin/Gyeongmin/Program.cs
+++ Gyeongmin/Gyeongmin/Gyeongmin/Program.cs
@@ -1,9 +1,10 @@
-﻿namespace Gyeongmin
+﻿using System;
+
+namespace HELLOWORLD
 {
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
         }
     }
 }
