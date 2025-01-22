af57e742a6bf45fd67766019c8ba8a63ba260125
DAL_Tohko <happywhrudals0203@gmail.com>
2025-01-22 20:57:32 +0900

    1-2

--- Gyeongmin/Gyeongmin/Gyeongmin/Program.cs
+++ Gyeongmin/Gyeongmin/Gyeongmin/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 
-namespace HELLOWORLD
+namespace baekjoon
 {
-    class Program{
-        static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+    class 사칙연산
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) * int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) / int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) % int.Parse(s[1]));
         }
     }
 }
